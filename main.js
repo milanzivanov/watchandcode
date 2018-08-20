3e5a1b0a3531673b05f2b913375ebaafa41cd283
milan.zivanov <milan.zivanov@gmail.com>
2018-08-20 17:02:42 +0200

    Version 6 - Thinking in Code

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ var todoList = {
             }
         }
 
-
     },
     addTodo: function(todoText) {
         this.todos.push({
@@ -37,17 +36,41 @@ var todoList = {
         var todo = this.todos[position];
         todo.complited = !todo.complited;
         this.displayTodos();
+    },
+    toggleAll: function() {
+        var totalTodos = this.todos.length;
+        var complitedTodos = 0;
+
+        // get number of complited todos
+        for(var i = 0; i < totalTodos; i++) {
+            if( this.todos[i].complited === true ) {
+                complitedTodos++;
+            }
+        }
+        // Case1: if everything true, make everything false 
+        if (complitedTodos === totalTodos) {
+            for(var i = 0; i < totalTodos; i++) {
+                this.todos[i].complited = false;
+            }
+        } 
+        // case2: otherwisemake everything true
+        else {
+            for(var i = 0; i < totalTodos; i++) {
+                this.todos[i].complited = true;
+            }
+        }
+
+        this.displayTodos();
     }
 };
-// todoList.displayTodos();
 // todoList.addTodos('adding item object');
 // todoList.changeTodos(1, "changed");
 // todoList.deliteTodos(0);
 
 todoList.addTodo("first");
 todoList.addTodo("second");
-todoList.addTodo("third");
 
-todoList.toggleComplited(1);
-todoList.toggleComplited(1);
+todoList.toggleAll();
+
 todoList.toggleComplited(0);
+todoList.toggleComplited(1);
